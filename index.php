5e8f4bbf05b7e3f26ec163615fce51dbf8c9ccb4
Samuel Githae <samgithae01@gmail.com>
2017-06-30 10:29:46 +0300

    changed views to en

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="refresh" content="0;url=views/index.php">
+<meta http-equiv="refresh" content="0;url=en/index.php">
 <title>Rep Sacco</title>
 <script language="javascript">
-    window.location.href = "views/index.php"
+    window.location.href = "en/index.php"
 </script>
 </head>
 <body>
-Go to <a href="views/index.php">/views/index.php</a>
+Go to <a href="en/index.php">/en/index.php</a>
 </body>
 </html>
