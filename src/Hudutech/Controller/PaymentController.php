56ec9839ee47ca2255f5124fb128927bdce0ea47
njerucyrus <njerucyrus5@hotmail.com>
2017-06-30 13:09:32 +0300

    completed order crud

---
+++ src/Hudutech/Controller/PaymentController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hudutech
+ * Date: 6/30/17
+ * Time: 11:53 AM
+ */
+
+namespace Hudutech\Controller;
+
+
+use Hudutech\AppInterface\PaymentInterface;
+use Hudutech\Entity\Payment;
+
+class PaymentController implements PaymentInterface
+{
+    public function create(Payment $payment)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public static function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public static function getId($id)
+    {
+        // TODO: Implement getId() method.
+    }
+
+    public static function getObject($id)
+    {
+        // TODO: Implement getObject() method.
+    }
+
+    public static function all()
+    {
+        // TODO: Implement all() method.
+    }
+
+
+}
